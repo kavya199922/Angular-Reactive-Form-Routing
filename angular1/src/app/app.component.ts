ca30f182f2f71dba721e84515a784ce493d477b6
kavya_R <kavya8428@gmail.com>
2020-08-06 00:39:52 +0530

    checking interpolation
    resolved issue:not to give template and templateURL in the same component

--- angular1/src/app/app.component.ts
+++ angular1/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 //view
 @Component({
   selector: 'app-root',
-  template:'<h1><strong>{{title}}</strong></h1>',
+  
   templateUrl: './app.component.html',
 
 })
 //data
 export class AppComponent {
-  title='app1'
+  
  
 }
