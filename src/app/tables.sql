48faf15f761f1b3c27bccf76f6b33aacf5e388ad
kavya_R <kavya8428@gmail.com>
2020-08-08 13:20:44 +0530

    added SQL queries

---
+++ src/app/tables.sql
@@ -0,0 +1,4 @@
+create table customerdata(customername varchar(20),custemail varchar(50),custpassword varchar(20));
+desc customerdata;
+insert into customerdata values('adhvaith','adhvaith34@gmail.com','12345adhu');
+select * from customerdata;
